b674387c20a3b40edc10e4a0842bed0a18f19343
eng.alaa.asaad <eng.alaa.asaad@ramaaz.dev>
2023-09-15 14:26:22 +0300

    fix

--- components/global/Animated.js
+++ components/global/Animated.js
@@ -5,12 +5,17 @@ import "@/styles/home.css"
 import { useDispatch, useSelector } from 'react-redux'
 import StoriesBar from '../Home/Stories';
 import { GetStoryData } from '@/redux/homepage/actions';
+import { getStoriesHeaders } from '@/utils/functions';
+import { GET_USERS_STORIES, STORIES_URL } from '@/utils/endpointConfig';
 function Animated({stories,res}) {
     console.log(res);
     const storiesData=useSelector((state)=>state.homepage.storiesData)
     const dispatch=useDispatch()
     useEffect(()=>{
         dispatch(GetStoryData(stories))
+        fetch(STORIES_URL+GET_USERS_STORIES,getStoriesHeaders()).then((res) => res.json()).then((data)=>{
+            console.log(data)
+        })
       },[]) 
   return (
     <div className='animated-container' style={{flexDirection:"column"}}>
