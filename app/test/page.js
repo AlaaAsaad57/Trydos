e53bf1410826acaa8132408fd166f82013f3518f
eng.alaa.asaad <eng.alaa.asaad@ramaaz.dev>
2023-09-15 05:27:11 +0300

    add animations

---
+++ app/test/page.js
@@ -0,0 +1,12 @@
+import Animated from '@/components/global/Animated'
+import React from 'react'
+
+function test() {
+  return (
+    <>
+       <Animated/> 
+    </>
+  )
+}
+
+export default test
---
+++ components/global/Animated.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import AnimatedSvg from "./AnimatedLogo.svg"
+import "@/styles/home.css"
+function Animated() {
+  return (
+    <div className='animated-container'>
+        <AnimatedSvg/>
+    </div>
+  )
+}
+
+export default Animated
