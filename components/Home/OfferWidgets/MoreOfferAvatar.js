1aebd722e41d94731bb48406db0f52315f3d3111
eng.alaa.asaad <eng.alaa.asaad@ramaaz.dev>
2023-11-16 18:51:14 -0600

    edit animation in offer element

--- components/Home/OfferWidgets/MoreOfferAvatar.js
+++ components/Home/OfferWidgets/MoreOfferAvatar.js
@@ -7,7 +7,7 @@ function MoreOfferAvatar({images,zIndex,viewed}) {
   const language=useSelector((state)=>state.homepage.language)
 
   return (
-    <div className='offer-avatar' style={{zIndex:zIndex,transform:`translateX(-${viewed*5}px)`}}>
+    <div className='offer-avatar hasMore' style={{zIndex:zIndex,transform:`translateX(-${viewed*5}px)`}}>
         <div className='offer-more-s'/>
         <span>{translate("More",language)}</span>
     <Image src={images} alt='avatar' width={40} height={40} style={{borderRadius:"50%",height:"40px"}}/>
