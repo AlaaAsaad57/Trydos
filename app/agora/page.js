619eb777f6580ecc267d7ae879616ada6a544681
eng.alaa.asaad <eng.alaa.asaad@ramaaz.dev>
2023-12-06 17:20:12 -0600

    fix

--- app/agora/page.js
+++ app/agora/page.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import App from '../../components/testCalls'
+import { SSRDetect } from '../../utils/functions'
 
 function AgoraTest() {
   return (
     <div>
-        <App/>
+     {SSRDetect()&&   <App/>}
     </div>
   )
 }
