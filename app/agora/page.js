e068ea1151effff79a55c16381fdaff56f8a7f35
eng.alaa.asaad <eng.alaa.asaad@ramaaz.dev>
2023-12-06 17:14:33 -0600

    add agora test calls

---
+++ app/agora/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import App from '../../components/testCalls'
+
+function AgoraTest() {
+  return (
+    <div>
+        <App/>
+    </div>
+  )
+}
+
+export default AgoraTest
