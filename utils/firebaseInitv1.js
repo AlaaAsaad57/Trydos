3e8db7c5e37bc65bf62f4934d9af054747dd0f90
eng.alaa.asaad <eng.alaa.asaad@ramaaz.dev>
2023-12-21 15:39:05 -0600

    fix

--- utils/firebaseInitv1.js
+++ utils/firebaseInitv1.js
@@ -47,8 +47,8 @@ export const onMessageListener = () =>
       }
      if(payload.data.type==="VoiceCallEvent"){
       let data=JSON.parse(payload.data.data).payload
-      let channel=store.getState().chat.data.filter((ch)=>parseInt(ch.id)===parseInt(data.channel_id))[0]?store.getState().chat.data.filter((ch)=>parseInt(ch.id)===parseInt(data.channel_id))[0]:{id:data.channel_id,messages:[{message_type:{name:'VoiceCall'}}],channel_members:[{user_id:data.user_id,user:store.getState().chat.contacts.filter((s)=>s.contact_user_id===data.user_id)[0],mute:0,pin:0,archived:0},{mute:0,pin:0,archived:0,user_id:getUserChat().id,user:getUserChat()}]}
-      let caller = store.getState().chat.data.filter((ch)=>parseInt(ch.id)===parseInt(data.channel_id)).length>0?store.getState().chat.data.filter((ch)=>parseInt(ch.id)===parseInt(data.channel_id))[0].channel_members.filter(one => one.user_id !== JSON.parse(localStorage.getItem("USER-CHAT")).id)[0]:{user:{name:data.name,photo_path:data.photo}}
+      let channel=store.getState().chat.data.filter((ch)=>parseInt(ch.id)===parseInt(data.channelId))[0]?store.getState().chat.data.filter((ch)=>parseInt(ch.id)===parseInt(data.channelId))[0]:{id:data.channelId,messages:[{message_type:{name:'VoiceCall'}}],channel_members:[{user_id:data.user_id,user:store.getState().chat.contacts.filter((s)=>s.contact_user_id===data.user_id)[0],mute:0,pin:0,archived:0},{mute:0,pin:0,archived:0,user_id:getUserChat().id,user:getUserChat()}]}
+      let caller = store.getState().chat.data.filter((ch)=>parseInt(ch.id)===parseInt(data.channelId)).length>0?store.getState().chat.data.filter((ch)=>parseInt(ch.id)===parseInt(data.channelId))[0].channel_members.filter(one => one.user_id !== JSON.parse(localStorage.getItem("USER-CHAT")).id)[0]:{user:{name:data.name,photo_path:data.photo}}
       if(data.user_id!==getUserChat().id&&(!store.getState().chat.callInProgress||store.getState().chat.callInProgress===2)){
         store.dispatch({ type: "INCOMING_VOICE_CALL",payload:{...data,callerChannel: channel,caller:caller,message_id:JSON.parse(payload.data.data).message.id} })
       }
@@ -75,8 +75,8 @@ export const onMessageListener = () =>
      }
      else if(payload.data.type==="VideoCallEvent"){
       let data=JSON.parse(payload.data.data).payload
-      let channel=store.getState().chat.data.filter((ch)=>parseInt(ch.id)===parseInt(data.channel_id))[0]?store.getState().chat.data.filter((ch)=>parseInt(ch.id)===parseInt(data.channel_id))[0]:{id:data.channel_id,messages:[{message_type:{name:'VoiceCall'}}],channel_members:[{user_id:data.user_id,user:store.getState().chat.contacts.filter((s)=>s.contact_user_id===data.user_id)[0],mute:0,pin:0,archived:0},{mute:0,pin:0,archived:0,user_id:getUserChat().id,user:getUserChat()}]}
-      let caller = store.getState().chat.data.filter((ch)=>parseInt(ch.id)===parseInt(data.channel_id)).length>0?store.getState().chat.data.filter((ch)=>parseInt(ch.id)===parseInt(data.channel_id))[0].channel_members.filter(one => one.user_id !== JSON.parse(localStorage.getItem("USER-CHAT")).id)[0]:{user:{name:data.name,photo_path:data.photo}}
+      let channel=store.getState().chat.data.filter((ch)=>parseInt(ch.id)===parseInt(data.channelId))[0]?store.getState().chat.data.filter((ch)=>parseInt(ch.id)===parseInt(data.channelId))[0]:{id:data.channelId,messages:[{message_type:{name:'VoiceCall'}}],channel_members:[{user_id:data.user_id,user:store.getState().chat.contacts.filter((s)=>s.contact_user_id===data.user_id)[0],mute:0,pin:0,archived:0},{mute:0,pin:0,archived:0,user_id:getUserChat().id,user:getUserChat()}]}
+      let caller = store.getState().chat.data.filter((ch)=>parseInt(ch.id)===parseInt(data.channelId)).length>0?store.getState().chat.data.filter((ch)=>parseInt(ch.id)===parseInt(data.channelId))[0].channel_members.filter(one => one.user_id !== JSON.parse(localStorage.getItem("USER-CHAT")).id)[0]:{user:{name:data.name,photo_path:data.photo}}
       if(data.user_id!==getUserChat().id&&(!store.getState().chat.callInProgress||store.getState().chat.callInProgress===2)){
       store.dispatch({ type: "INCOMING_CALL",payload:{...data,callerChannel: channel,caller:caller,message_id:JSON.parse(payload.data.data).message.id} })}
         store.dispatch({type:"SET_LAST_NOTIFICATION_DATE",payload:(new Date()).toLocaleString()})
