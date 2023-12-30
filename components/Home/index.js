18071512849b6a2252c68caa93de7922064942f0
eng.alaa.asaad <eng.alaa.asaad@ramaaz.dev>
2023-12-30 08:21:56 -0600

    fix

--- components/Home/index.js
+++ components/Home/index.js
@@ -49,7 +49,7 @@ export default function Home({stories,HomeData_res,stories_res,HomeData}) {
     })
     typeof window !=='undefined'&& 'serviceWorker' in navigator&& onMessageListener().then(payload => {
       if(process.env.NEXT_PUBLIC_ENABLE_LOG==='true') console.log(payload)
-   }).catch(err => if(process.env.NEXT_PUBLIC_ENABLE_LOG==='true') console.log('failed: ', err));
+   }).catch(err => {if(process.env.NEXT_PUBLIC_ENABLE_LOG==='true') console.log('failed: ', err)});
   }catch(e){
     if(process.env.NEXT_PUBLIC_ENABLE_LOG==='true') console.log(e)
   } 
