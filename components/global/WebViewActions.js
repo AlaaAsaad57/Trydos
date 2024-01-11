0b6cf29968d70b302a97fec8fa962531e3826bee
eng.alaa.asaad <eng.alaa.asaad@ramaaz.dev>
2024-01-11 16:24:40 -0600

    fix

--- components/global/WebViewActions.js
+++ components/global/WebViewActions.js
@@ -28,7 +28,7 @@ export const getAgoraToken =async (channel_id,token,mid)=>{
        alert(data.data.data?.length>0)
     })
     if(!status)
-    await AnswerWebView(token,mid);
+    // await AnswerWebView(token,mid);
     return [tok,status]
 }
 export const getAgoraTokenForInit=async(channel_id,token,mid)=>{
