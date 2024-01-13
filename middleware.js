c42c4fa3a39c6dde0cb0f657a3f49954c2866a81
eng.alaa.asaad <eng.alaa.asaad@ramaaz.dev>
2024-01-13 06:51:31 -0600

    fix

--- middleware.js
+++ middleware.js
@@ -121,6 +121,6 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      */
-    "/((?!api|static|.*\\..*|_next|endCall|call_direct|revalidate).*)",
+    "/((?!api|static|.*\\..*|_next|endCall|call_direct|revalidate|callInProg).*)",
   ],
 };
