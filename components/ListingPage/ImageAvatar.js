7988cb13b004ea51cb3c8b42586b358ace77fa66
eng.alaa.asaad <eng.alaa.asaad@ramaaz.dev>
2024-01-13 17:40:06 -0600

    fix

--- components/ListingPage/ImageAvatar.js
+++ components/ListingPage/ImageAvatar.js
@@ -1,34 +1,67 @@
-import Image from 'next/image'
-import React, {memo} from 'react'
+import Image from "next/image";
+import React, { memo } from "react";
 
-function ImageAvatar({image,width,height,alt,isActive,name}) {    
+function ImageAvatar({ image, width, height, alt, isActive, name }) {
   return (
-    <div className='image-avatar'>
-
-        {isActive?
-            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 35 35">
-  <g id="Ellipse_5" data-name="Ellipse 5" fill="none" stroke={name==='blue'?"#0048AC":name} strokeWidth="0.5">
-    <circle cx="50%" cy="50%" r="50%" stroke="none"/>
-    <circle cx="50%" cy="50%" r="50%" fill="none"/>
-  </g>
-</svg>
-:
-<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 35 35">
-<g id="Ellipse_5" data-name="Ellipse 5" fill="none" stroke="#FFFFFF" strokeWidth="0.5">
-  <circle cx="50%" cy="50%" r="50%" stroke="none"/>
-  <circle cx="50%" cy="50%" r="50%" fill="none"/>
-</g>
-</svg>      
-    }
-    {isActive&&
-    <div className='avatar-text-element' style={{color:name}}>
-        {name}
+    <div className="image-avatar">
+      {isActive ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="100%"
+          height="100%"
+          viewBox="0 0 35 35"
+        >
+          <g
+            id="Ellipse_5"
+            data-name="Ellipse 5"
+            fill="none"
+            stroke={name === "blue" ? "#0048AC" : name}
+            strokeWidth="0.5"
+          >
+            <circle cx="50%" cy="50%" r="50%" stroke="none" />
+            <circle cx="50%" cy="50%" r="50%" fill="none" />
+          </g>
+        </svg>
+      ) : (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="100%"
+          height="100%"
+          viewBox="0 0 35 35"
+        >
+          <g
+            id="Ellipse_5"
+            data-name="Ellipse 5"
+            fill="none"
+            stroke="#FFFFFF"
+            strokeWidth="0.5"
+          >
+            <circle cx="50%" cy="50%" r="50%" stroke="none" />
+            <circle cx="50%" cy="50%" r="50%" fill="none" />
+          </g>
+        </svg>
+      )}
+      {isActive && (
+        <div className="avatar-text-element" style={{ color: name }}>
+          {name}
+        </div>
+      )}
+      <div className="shadow-inset-avatar" />
+      <Image
+        quality={80}
+        src={image}
+        fill
+        alt={alt || "alt"}
+        style={{
+          borderRadius: "50%",
+          zIndex: "3",
+          height: "100%",
+          objectPosition: "center top",
+          objectFit: "cover",
+        }}
+      />
     </div>
-    }
-    <div className='shadow-inset-avatar'/>
-        <Image  quality={80} src={image} width={width} height={height} alt={alt||'alt'} style={{borderRadius:"50%",zIndex:'3',objectPosition:'center top',objectFit:'cover'}}/>
-    </div>
-  )
+  );
 }
 
-export default memo(ImageAvatar)
+export default memo(ImageAvatar);
