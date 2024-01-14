5dddecde8873519096ce6f6961c1ea8fe0c30330
eng.alaa.asaad <eng.alaa.asaad@ramaaz.dev>
2024-01-14 10:12:18 -0600

    add sentry

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const withSvgr = require("next-svgr");
-
-module.exports = withSvgr({
+const { withSentryConfig } = require("@sentry/nextjs");
+const nextConfig = withSvgr({
   images: {
     domains: [
       "res.cloudinary.com",
@@ -23,3 +23,19 @@ module.exports = withSvgr({
   },
   // your config for other plugins or the general next.js here...
 });
+const sentryWebpackPluginOptions = {
+  // Additional config options for the Sentry webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, configFile, stripPrefix, urlPrefix, include, ignore
+  // org: "example-org",
+  // project: "duttip",
+  // // An auth token is required for uploading source maps.
+  // authToken: process.env.SENTRY_AUTH_TOKEN,
+  // silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+// Make sure adding Sentry options is the last code to run before exporting
+module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
