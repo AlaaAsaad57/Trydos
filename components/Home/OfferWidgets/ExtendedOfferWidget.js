65da70456972b47c5ac2e1f926fc92ac859032a1
eng.alaa.asaad <eng.alaa.asaad@ramaaz.dev>
2024-03-09 05:12:23 -0600

    fix

--- components/Home/OfferWidgets/ExtendedOfferWidget.js
+++ components/Home/OfferWidgets/ExtendedOfferWidget.js
@@ -44,7 +44,7 @@ function ExtendedOfferWidget({ offer, myKey, onClick }) {
             objectFit="cover"
             objectPosition="center"
             src={
-              "https://res.cloudinary.com/djooohujg/image/upload/q_50/w_800/f_auto/1708506792?_a=DdATC1RAAZAA0&w=800&q=60"
+              "https://res.cloudinary.com/djooohujg/image/upload/q_50/w_800/f_webp/1708506792?_a=DdATC1RAAZAA0&w=800&q=60"
             }
           />{" "}
           <div className="offer-blured-background" />
--- components/Home/OfferWidgets/OfferAvatars.js
+++ components/Home/OfferWidgets/OfferAvatars.js
@@ -73,7 +73,7 @@ function OfferAvatars({ priority }) {
       <MoreOfferAvatar
         priority={priority}
         images={
-          "https://res.cloudinary.com/djooohujg/image/upload/f_auto/q_auto/1707907006?_a=DATC1RAAZAA0"
+          "https://res.cloudinary.com/djooohujg/image/upload/f_webp/q_auto/1707907006?_a=DATC1RAAZAA0"
         }
         zIndex={100}
         viewed={5}
