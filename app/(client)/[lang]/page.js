fe1a4289fdd822acfd004eddbf3e7c102e11013c
eng.alaa.asaad <eng.alaa.asaad@ramaaz.dev>
2024-03-10 04:29:58 -0500

    fix

--- app/(client)/[lang]/page.js
+++ app/(client)/[lang]/page.js
@@ -1,11 +1,12 @@
 "use server";
 import Home from "components/Home";
-// import { getHomeData } from "store/homepage/cachedActions";
+import { getHomeData } from "store/homepage/cachedActions";
 
 async function page() {
+  const [HomeData, HomeData_res] = await getHomeData();
   return (
     <>
-      <Home HomeData_res={[]} HomeData={[]} />
+      <Home HomeData_res={HomeData_res} HomeData={HomeData} />
     </>
   );
 }
