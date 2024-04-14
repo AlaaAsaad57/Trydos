a1273a01e83e886393e86ce307cb379d31ec6202
eng.alaa.asaad <eng.alaa.asaad@ramaaz.dev>
2024-04-14 05:18:58 -0500

    merged hooks branch

---
+++ components/Providers/ReactQueryClientProvider.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React, { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+export const ReactQueryClientProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [client] = useState(new QueryClient());
+  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+};
