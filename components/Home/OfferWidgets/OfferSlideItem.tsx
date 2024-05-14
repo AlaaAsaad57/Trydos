90b7f0535c1ad9e4a8f337cc26496b9919537a91
eng.alaa.asaad <eng.alaa.asaad@ramaaz.dev>
2024-05-14 02:41:45 -0500

    fix

--- components/Home/OfferWidgets/OfferSlideItem.tsx
+++ components/Home/OfferWidgets/OfferSlideItem.tsx
@@ -20,7 +20,7 @@ function OfferSlideItem({ isSingle, priority }: OfferSlideItemProps) {
           fetchPriority={priority ? "high" : "low"}
           className="OfferImage"
           src={
-            "https://res.cloudinary.com/djooohujg/image/upload/f_webp/q_auto/1708506792?_a=DATC1RAAZAA0"
+            "https://res.cloudinary.com/djooohujg/image/upload/f_webp/q_10/1708506792?_a=DATC1RAAZAA0"
           }
           width={360}
           height={155}
