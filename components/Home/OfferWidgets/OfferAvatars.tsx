f6f98f1f570d5834db56b6491f70bb9ba09d1ae4
eng.alaa.asaad <eng.alaa.asaad@ramaaz.dev>
2024-05-14 02:37:14 -0500

    fix

--- components/Home/OfferWidgets/OfferAvatars.tsx
+++ components/Home/OfferWidgets/OfferAvatars.tsx
@@ -71,14 +71,14 @@ function OfferAvatars({ priority }: OfferAvatarsProps) {
       />
       <OfferAvatar
         images={
-          "https://res.cloudinary.com/djooohujg/image/upload/f_webp/q_50/1707907006?_a=DATC1RAAZAA0"
+          "https://res.cloudinary.com/djooohujg/image/upload/f_webp/q_10/1707907006?_a=DATC1RAAZAA0"
         }
         zIndex={5}
       />
       <MoreOfferAvatar
         priority={false}
         images={
-          "https://res.cloudinary.com/djooohujg/image/upload/f_webp/q_auto/1707907006?_a=DATC1RAAZAA0"
+          "https://res.cloudinary.com/djooohujg/image/upload/f_webp/q_10/1707907006?_a=DATC1RAAZAA0"
         }
         zIndex={100}
         viewed={5}
