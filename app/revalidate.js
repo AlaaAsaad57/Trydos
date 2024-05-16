aa1985cae2c9467601bfb59d39d5ca2b72d179ba
eng.alaa.asaad <eng.alaa.asaad@ramaaz.dev>
2024-05-16 11:18:10 -0500

    fix

--- app/revalidate.js
+++ app/revalidate.js
@@ -2,7 +2,8 @@
 import { revalidatePath, revalidateTag } from "next/cache";
 
 export default async function Revalidate() {
-  revalidatePath("/");
+  revalidatePath("/", "layout");
+  revalidatePath("/", "page");
   revalidateTag("home-boutiques"); // Update cached posts
   revalidateTag("stories"); // Update cached posts
   revalidateTag("listing-data"); // Update cached posts
