b9ba3a7fc7d3599dc9965e838b5113fdb4026c76
eng.alaa.asaad <eng.alaa.asaad@ramaaz.dev>
2024-05-17 05:24:39 -0500

    fix

--- app/(client)/[lang]/error.tsx
+++ app/(client)/[lang]/error.tsx
@@ -2,8 +2,6 @@
 
 import { useEffect } from "react";
 import Logo from "components/Home/Logo";
-import axios from "axios";
-
 export default function Error({
   error,
   reset,
@@ -28,6 +26,7 @@ export default function Error({
         ? JSON.parse(localStorage.getItem("USER-CHAT")).access_token
         : null;
     }
+    let axios = (await import("axios")).default;
     axios.post(`${baseUrl}/mobile_error_log/store`, {
       error_description: error.message,
       last_json,
