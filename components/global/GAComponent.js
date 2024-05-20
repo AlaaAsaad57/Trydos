4cb70f68f92e5ac4c3b77adc24fdcda26da848d7
eng.alaa.asaad <eng.alaa.asaad@ramaaz.dev>
2024-05-20 07:14:36 -0500

    fix

--- components/global/GAComponent.js
+++ components/global/GAComponent.js
@@ -3,10 +3,9 @@ import { useEffect } from "react";
 import { GoogleAnalytics } from "nextjs-google-analytics";
 function GAComponent() {
   useEffect(() => {}, []);
-  let GA_MEASUREMENT_ID = "G-EK7TKN11PV";
   return (
     <>
-      <GoogleAnalytics strategy="lazyOnload" trackPageViews />
+      <GoogleAnalytics strategy="worker" trackPageViews />
     </>
   );
 }
