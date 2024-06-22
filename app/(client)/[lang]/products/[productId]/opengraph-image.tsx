889f97688374d0f183eda85455dbcb35c4d18600
eng.alaa.asaad <eng.alaa.asaad@ramaaz.dev>
2024-06-22 03:55:11 -0500

    fix

--- app/(client)/[lang]/products/[productId]/opengraph-image.tsx
+++ app/(client)/[lang]/products/[productId]/opengraph-image.tsx
@@ -4,8 +4,8 @@ import LogoAuth from "public/svg/LogoAuth.svg";
 import { ImageResponse } from "next/og";
 import { getProductDataOG } from "store/homepage/cachedActions";
 export const size = {
-  width: 200,
-  height: 200,
+  width: 1200,
+  height: 630,
 };
 export const alt = "TryDos";
 export const contentType = "image/png";
