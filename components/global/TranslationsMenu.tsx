3e9e6e21f6bfefbfd128c6f12b3ba84b5aa72d69
eng.alaa.asaad <eng.alaa.asaad@ramaaz.dev>
2024-08-20 06:41:36 -0500

    fix

--- components/global/TranslationsMenu.tsx
+++ components/global/TranslationsMenu.tsx
@@ -26,74 +26,76 @@ function TranslationsMenu({ init }: TranslationsMenuProps) {
   return (
     <div className="translations-container">
       {language === "ar" && <ArabicCss />}
-      <div className="translations-container-inner">
-        <div className="translation-icon">
-          <img
-            src={"/svg/translations.svg"}
-            width={30}
-            height={30}
-            alt="language"
-          />
-        </div>
+      {false && (
+        <div className="translations-container-inner">
+          <div className="translation-icon">
+            <img
+              src={"/svg/translations.svg"}
+              width={30}
+              height={30}
+              alt="language"
+            />
+          </div>
 
-        <div
-          className={`translation-icon cursor-pointer tr-icon ${
-            language === "tr" && "selected-language"
-          }`}
-          onClick={() => {
-            dispatch(changeAppLanguage("tr"));
-            window.location.href = window.location.href.replace(
-              init,
-              `${init.split("-")[0]}-tr`
-            );
-          }}
-        >
-          <img
-            src={"/svg/tr.svg"}
-            width={30}
-            height={20}
-            alt="turkish language"
-          />
+          <div
+            className={`translation-icon cursor-pointer tr-icon ${
+              language === "tr" && "selected-language"
+            }`}
+            onClick={() => {
+              dispatch(changeAppLanguage("tr"));
+              window.location.href = window.location.href.replace(
+                init,
+                `${init.split("-")[0]}-tr`
+              );
+            }}
+          >
+            <img
+              src={"/svg/tr.svg"}
+              width={30}
+              height={20}
+              alt="turkish language"
+            />
+          </div>
+          <div
+            className={`translation-icon cursor-pointer en-icon ${
+              language === "en" && "selected-language"
+            }`}
+            onClick={() => {
+              dispatch(changeAppLanguage("en"));
+              window.location.href = window.location.href.replace(
+                init,
+                `${init.split("-")[0]}-en`
+              );
+            }}
+          >
+            <img
+              src={"/svg/uk.svg"}
+              width={30}
+              height={20}
+              alt="english language"
+            />
+          </div>
+          <div
+            className={`translation-icon cursor-pointer ar-icon ${
+              language === "ar" && "selected-language"
+            }`}
+            onClick={() => {
+              dispatch(changeAppLanguage("ar"));
+              window.location.href = window.location.href.replace(
+                init,
+                `${init.split("-")[0]}-ar`
+              );
+            }}
+          >
+            <img
+              src={"/svg/uae.svg"}
+              width={30}
+              height={20}
+              alt="arabic language"
+            />
+          </div>
         </div>
-        <div
-          className={`translation-icon cursor-pointer en-icon ${
-            language === "en" && "selected-language"
-          }`}
-          onClick={() => {
-            dispatch(changeAppLanguage("en"));
-            window.location.href = window.location.href.replace(
-              init,
-              `${init.split("-")[0]}-en`
-            );
-          }}
-        >
-          <img
-            src={"/svg/uk.svg"}
-            width={30}
-            height={20}
-            alt="english language"
-          />
-        </div>
-        <div
-          className={`translation-icon cursor-pointer ar-icon ${
-            language === "ar" && "selected-language"
-          }`}
-          onClick={() => {
-            dispatch(changeAppLanguage("ar"));
-            window.location.href = window.location.href.replace(
-              init,
-              `${init.split("-")[0]}-ar`
-            );
-          }}
-        >
-          <img
-            src={"/svg/uae.svg"}
-            width={30}
-            height={20}
-            alt="arabic language"
-          />
-        </div>
-      </div>
+      )}
     </div>
   );
 }
