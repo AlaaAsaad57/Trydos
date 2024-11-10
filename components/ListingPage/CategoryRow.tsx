e50b8ece8f5a5830655860fe6b4fea289d32e9db
AlaaAsaadDev <alaa.asaad@ramaaz.com>
2024-11-10 09:04:33 -0600

    show 2 levels of categories

--- components/ListingPage/CategoryRow.tsx
+++ components/ListingPage/CategoryRow.tsx
@@ -5,11 +5,19 @@ import { useSelector } from "react-redux";
 
 function CategoryRow() {
   const filters = useSelector((state: any) => state.details.filters);
-
+  const configureCategory = (category) => {
+    let obj = { ...category, childes: [...(category?.childes || [])] };
+    category.childes?.map((s) => {
+      s.childes?.map((sub) => {
+        obj.childes.push(sub);
+      });
+    });
+    return obj;
+  };
   return (
     <div className="category-row-container flex-row">
       {filters.categories.map((category, key) => (
-        <CategoryCircle key={key} category={category} />
+        <CategoryCircle key={key} category={configureCategory(category)} />
       ))}
     </div>
   );
