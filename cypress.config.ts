9f9e9c2bdcf9450b7531c3bda5ca49f7d54bf510
AlaaAsaadDev <alaa.asaad@ramaaz.com>
2024-11-11 07:17:36 -0600

    edit base url

--- cypress.config.ts
+++ cypress.config.ts
@@ -22,8 +22,9 @@ export default defineConfig({
       require("cypress-mochawesome-reporter/plugin")(on);
       // implement node event listeners here
     },
-    // baseUrl: "http://127.0.0.1:3000",
-    baseUrl: "http://localhost:3000",
+    baseUrl:
+      "https://trydos-front-git-development-trydos-front-team.vercel.app/",
+    // baseUrl: "http://localhost:3000",
     testIsolation: false,
     env: {
       browserPermissions: {
