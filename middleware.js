71ef45754a8853b29d9dbd7c69f886911000687d
AlaaAsaadDev <alaa.asaad@ramaaz.com>
2025-01-18 01:51:55 +0300

    edit middleware

--- middleware.js
+++ middleware.js
@@ -1,6 +1,5 @@
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
-import { NextResponse } from "next/server";
 import { getCountriesApi } from "./store/homepage/cachedActions";
 import axios from "axios";
 
