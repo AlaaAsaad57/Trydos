cb1cdb1eb7f9ee6d7c5c79f289e50740a5955b14
AlaaAsaadDev <alaa.asaad@ramaaz.com>
2025-03-20 15:03:49 +0300

    Update Cypress configuration to enable experimental features, including experimentalRunAllSpecs, for improved testing capabilities and flexibility.

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   projectId: "d1rk7o",
   chromeWebSecurity: false,
   reporter: "cypress-mochawesome-reporter",
+
   reporterOptions: {
     // To display small circular charts regarding test results
     embeddedScreenshots: true,
@@ -23,6 +24,7 @@ export default defineConfig({
     // @ts-ignore
     hideXHRInCommandLog: true,
     experimentalStudio: true,
+    experimentalRunAllSpecs: true,
     setupNodeEvents(on, config) {
       TS(on, config);
       require("cypress-mochawesome-reporter/plugin")(on);
