0e532f4f9f77860e36b920dd321e6137048d9c31
AlaaAsaadDev <alaa.asaad@ramaaz.com>
2025-03-25 02:48:05 +0300

    Refactor Menu component to streamline logout functionality. Simplify conditional rendering of the logout MenuItem by removing redundant code. Maintain user experience by ensuring the logout option is displayed only when a user is logged in.

--- components/Home/Menu.tsx
+++ components/Home/Menu.tsx
@@ -313,22 +313,23 @@ const Menu: React.FC<MenuProps> = ({ user }) => {
             {translateFunction("Compare")}
           </MenuItem>
         </>
+        {user && (
+          <MenuItem
+            dataCy="logout"
+            onClick={handleLogout}
+            icon={
+              <MenuIcon>
+                <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" />
+                <polyline points="16 17 21 12 16 7" />
+                <line x1="21" y1="12" x2="9" y2="12" />
+              </MenuIcon>
+            }
+          >
+            {translateFunction("Logout")}
+          </MenuItem>
+        )}
       </div>
-      {user && (
-        <MenuItem
-          dataCy="logout"
-          onClick={handleLogout}
-          icon={
-            <MenuIcon>
-              <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" />
-              <polyline points="16 17 21 12 16 7" />
-              <line x1="21" y1="12" x2="9" y2="12" />
-            </MenuIcon>
-          }
-        >
-          {translateFunction("Logout")}
-        </MenuItem>
-      )}
+
       {showNotifications && (
         <NotificationsPanel onClose={() => setShowNotifications(false)} />
       )}
