988d0c3602569eafff4038a98bb7c42e4fa5ab9e
AlaaAsaadDev <alaa.asaad@ramaaz.com>
2025-05-08 12:57:50 +0300

    Implement ProductBackButton with navigation and referrer check
    - Added a new `ProductBackButton` component that utilizes `NextLink` for navigation.
    - Implemented a check for document referrer to conditionally render the back button.
    - Enhanced user experience by ensuring the back button only appears when navigating from outside the current origin.

--- components/products/ProductBackButton.tsx
+++ components/products/ProductBackButton.tsx
@@ -1,8 +1,24 @@
 "use client";
 import React from "react";
 import BackIcon from "public/svg/listing/backIcon.svg";
+import NextLink from "components/global/NextLink";
+import { useParams } from "next/navigation";
 
 function ProductBackButton() {
+  const { lang } = useParams();
+  if (!document.referrer.includes(window.location.origin))
+    return (
+      <div className="back-bar align-center w-100 flex-row">
+        <NextLink
+          data-cy="backIcon_productPage"
+          className={`back-icon flex-row`}
+          href={`/${lang}`}
+          data={{ is_full_home: true }}
+        >
+          <BackIcon />
+        </NextLink>
+      </div>
+    );
   return (
     <div className="back-bar align-center w-100 flex-row">
       <div
