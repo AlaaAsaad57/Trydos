8c3e0f8294610ea7559da248d2a18ac9fd293a71
AlaaAsaadDev <alaa.asaad@ramaaz.com>
2025-05-30 18:02:00 +0300

    fix(CartProvider): update coupon and selected URL parameter handling
    - Refactored the `CartProvider` component to improve the handling of `coupon` and `selected` URL parameters.
    - Replaced the variable `a` with `couponUrlVar` for clarity and added logic to remove the `selected` parameter from the URL when present, enhancing the overall functionality and maintainability of the component.

--- components/Cart/CartProvider.tsx
+++ components/Cart/CartProvider.tsx
@@ -97,13 +97,18 @@ const CartProvider = () => {
     if (searchParams.get("cart")) {
       enableCartAction(true);
     }
-    let a = searchParams.get("coupon");
-
+    let couponUrlVar = searchParams.get("coupon");
+    let selectedUrlVar = searchParams.get("selected");
     setTimeout(() => {
-      if (a) {
-        localStorage.setItem("coupon-number", a);
+      if (couponUrlVar) {
+        localStorage.setItem("coupon-number", couponUrlVar);
         const newParams = new URLSearchParams(searchParams);
         newParams.delete("coupon");
+        router.replace(newParams.size ? `${pathname}?${newParams}` : pathname);
+      }
+      if (selectedUrlVar) {
+        const newParams = new URLSearchParams(searchParams);
+        newParams.delete("selected");
         router.replace(newParams.size ? `${pathname}?${newParams}` : pathname);
       }
     }, 1000);
