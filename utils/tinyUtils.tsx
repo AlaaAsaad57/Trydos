05639a932c0872ef0f8cf9578c25f0e1404a8eb3
AlaaAsaadDev <alaa.asaad@ramaaz.com>
2025-06-17 11:17:53 +0300

    Enhance GetImageUrl utility to ensure safe handling of undefined URLs by adding a null check. This improves robustness in image URL generation.

--- utils/tinyUtils.tsx
+++ utils/tinyUtils.tsx
@@ -331,6 +331,7 @@ export const GetAddressString = (location) => {
   return str;
 };
 export const GetImageUrl = (url) => {
-  if (url?.includes("http")) return url;
+  if (!url) return url;
+  if (url && url?.includes("http")) return url;
   return process.env.NEXT_PUBLIC_BASE_CLOUDINARY_URL + url;
 };
