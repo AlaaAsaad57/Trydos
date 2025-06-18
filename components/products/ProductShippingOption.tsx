43855faffcb64285852441c715ff621b37808b36
AlaaAsaadDev <alaa.asaad@ramaaz.com>
2025-06-18 12:34:32 +0300

    feat: Add formatTimeEdited function to display formatted shipping time in ProductShippingOption component

--- components/products/ProductShippingOption.tsx
+++ components/products/ProductShippingOption.tsx
@@ -44,7 +44,69 @@ function ProductShippingOption({ days }) {
   const translate = (key, lang?) => {
     return translateFunction(key, languageVariable);
   };
+  const formatTimeEdited = (timeString: string) => {
+    const MONTH_NAMES = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    const date = new Date(timeString);
+
+    const today = new Date();
+    const yesterday = new Date(today);
+    yesterday.setDate(yesterday.getDate() - 1);
+
+    const hours = date.getHours().toString().padStart(2, "0");
+    const minutes = date.getMinutes().toString().padStart(2, "0");
+    const seconds = date.getSeconds().toString().padStart(2, "0");
+    const timeFormat = `${hours}:${minutes}:${seconds}`;
+
+    if (date.toDateString() === today.toDateString()) {
+      return `Today | ${timeFormat}`;
+    }
+
+    if (date.toDateString() === yesterday.toDateString()) {
+      return `Yesterday | ${timeFormat}`;
+    }
+
+    const isSameYear = date.getFullYear() === today.getFullYear();
+    const isNewerThanToday = date > today;
+
+    if (isSameYear && isNewerThanToday) {
+      const day = date.getDate();
+      const monthName = MONTH_NAMES[date.getMonth()];
+      return `${day} ${monthName}`;
+    }
+
+    const day = date.getDate().toString().padStart(2, "0");
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
+    const year = date.getFullYear();
+
+    return `${day}/${month}/${year} | ${timeFormat}`;
+  };
   useEffect(() => {
+    console.log({
+      days,
+      formatted_time: formatTime(
+        new Date(
+          new Date().getTime() + Number(days || 0) * 24 * 60 * 60 * 1000
+        ).toString()
+      ),
+      formatted_time_edited: formatTimeEdited(
+        new Date(
+          new Date().getTime() + Number(days || 0) * 24 * 60 * 60 * 1000
+        ).toString()
+      ),
+    });
     getCountries();
   }, []);
   const [extended, setExtended] = useState(false);
