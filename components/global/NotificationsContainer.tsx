ce1f2d0d3f1facd34c391b56f159ddffc32e2ae9
AlaaAsaadDev <alaa.asaad@ramaaz.com>
2025-06-25 18:55:57 +0300

    fix sitemap and notifications

--- components/global/NotificationsContainer.tsx
+++ components/global/NotificationsContainer.tsx
@@ -30,7 +30,7 @@ const NotificationsContainer = () => {
       if (!timers[notification.id] && !dismissingIds.has(notification.id)) {
         timers[notification.id] = setTimeout(() => {
           handleDismiss(notification.id);
-        }, notification.duration || 5000);
+        }, 50000);
       }
     });
 
@@ -76,9 +76,9 @@ const NotificationsContainer = () => {
         `,
         }}
       />
-      <div className="fixed top-4 right-4 z-[9999999999999999999] flex flex-col gap-3 pointer-events-none w-[90%]">
-        {notifications.map((notification) => {
-          const notificationClasses = `
+
+      {notifications.map((notification, index) => {
+        const notificationClasses = `
             relative w-full max-w-[1200px] px-4 py-3 rounded-[15px] shadow-lg
             transform transition-all duration-300 ease-out
             pointer-events-auto
@@ -96,138 +96,145 @@ const NotificationsContainer = () => {
             ${notification.href ? " cursor-pointer" : ""}
           `;
 
-          const notificationContent = (
-            <div className="flex items-start gap-3 relative">
-              {/* Icon */}
-              <div className="flex-shrink-0">
-                {notification?.image ? (
-                  <Image
-                    className="object-cover object-center max-w-[40px] max-h-[40px] w-full h-full"
-                    alt={notification.message}
-                    src={GetImageUrl(notification.image)}
-                    width={40}
-                    height={40}
-                  />
-                ) : (
-                  <NotificationIcon />
-                )}
-              </div>
+        const notificationContent = (
+          <div className="flex items-start gap-3 relative">
+            {/* Icon */}
+            <div className="flex-shrink-0">
+              {notification?.image ? (
+                <Image
+                  className="object-cover object-center max-w-[40px] max-h-[40px] w-full h-full"
+                  alt={notification.message}
+                  src={GetImageUrl(notification.image)}
+                  width={40}
+                  height={40}
+                />
+              ) : (
+                <NotificationIcon />
+              )}
+            </div>
 
-              {/* Message */}
-              <div className="flex-1 flex-col gap-[4px]">
-                <p className="text-[12px] regular text-[#402CDD]">
-                  {translateFunction("Info Message ")}
-                </p>
-                <p className={`text-[12px] regular text-[#402CDD] `}>
-                  {notification.message}
-                </p>
-              </div>
+            {/* Message */}
+            <div className="flex-1 flex-col gap-[4px]">
+              <p className="text-[12px] regular text-[#402CDD]">
+                {translateFunction("Info Message ")}
+              </p>
+              <p className={`text-[12px] regular text-[#402CDD] `}>
+                {notification.message}
+              </p>
+            </div>
 
-              {/* Close button */}
-              <button
-                onClick={() => handleDismiss(notification.id)}
-                className={`
+            {/* Close button */}
+            <button
+              onClick={() => handleDismiss(notification.id)}
+              className={`
                   flex-shrink-0 p-1 absolute right-[-30px] top-[-23px] rounded-md transition-colors
                   
                 `}
-                tabIndex={0}
-                aria-label="Close notification"
+              tabIndex={0}
+              aria-label="Close notification"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                width="36"
+                height="36"
+                viewBox="0 0 36 36"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  xmlnsXlink="http://www.w3.org/1999/xlink"
-                  width="36"
-                  height="36"
-                  viewBox="0 0 36 36"
+                <defs>
+                  <filter
+                    id="Ellipse_274"
+                    x="0"
+                    y="0"
+                    width="36"
+                    height="36"
+                    filterUnits="userSpaceOnUse"
+                  >
+                    <feOffset dy="3" />
+                    <feGaussianBlur stdDeviation="3" result="blur" />
+                    <feFlood flood-opacity="0.102" />
+                    <feComposite operator="in" in2="blur" />
+                    <feComposite in="SourceGraphic" />
+                  </filter>
+                </defs>
+                <g
+                  id="Group_12369"
+                  data-name="Group 12369"
+                  transform="translate(9.239 6)"
                 >
-                  <defs>
-                    <filter
-                      id="Ellipse_274"
-                      x="0"
-                      y="0"
-                      width="36"
-                      height="36"
-                      filterUnits="userSpaceOnUse"
-                    >
-                      <feOffset dy="3" />
-                      <feGaussianBlur stdDeviation="3" result="blur" />
-                      <feFlood flood-opacity="0.102" />
-                      <feComposite operator="in" in2="blur" />
-                      <feComposite in="SourceGraphic" />
-                    </filter>
-                  </defs>
                   <g
-                    id="Group_12369"
-                    data-name="Group 12369"
-                    transform="translate(9.239 6)"
+                    transform="matrix(1, 0, 0, 1, -9.24, -6)"
+                    filter="url(#Ellipse_274)"
                   >
                     <g
-                      transform="matrix(1, 0, 0, 1, -9.24, -6)"
-                      filter="url(#Ellipse_274)"
+                      id="Ellipse_274-2"
+                      data-name="Ellipse 274"
+                      transform="translate(9 6)"
+                      fill="#fff"
+                      stroke="#402cdd"
+                      stroke-width="0.3"
                     >
-                      <g
-                        id="Ellipse_274-2"
-                        data-name="Ellipse 274"
-                        transform="translate(9 6)"
-                        fill="#fff"
-                        stroke="#402cdd"
-                        stroke-width="0.3"
-                      >
-                        <circle cx="9" cy="9" r="9" stroke="none" />
-                        <circle cx="9" cy="9" r="8.85" fill="none" />
-                      </g>
-                    </g>
-                    <g
-                      id="Group_12328"
-                      data-name="Group 12328"
-                      transform="translate(4.184 4.264)"
-                    >
-                      <line
-                        id="Line_888"
-                        data-name="Line 888"
-                        x2="12.851"
-                        transform="matrix(0.695, -0.719, 0.719, 0.695, 0.158, 9.244)"
-                        fill="none"
-                        stroke="#402cdd"
-                        stroke-linecap="round"
-                        stroke-width="1"
-                      />
-                      <line
-                        id="Line_889"
-                        data-name="Line 889"
-                        x2="12.851"
-                        transform="matrix(0.719, 0.695, -0.695, 0.719, 0, 0.158)"
-                        fill="none"
-                        stroke="#402cdd"
-                        stroke-linecap="round"
-                        stroke-width="1"
-                      />
+                      <circle cx="9" cy="9" r="9" stroke="none" />
+                      <circle cx="9" cy="9" r="8.85" fill="none" />
                     </g>
                   </g>
-                </svg>
-              </button>
-            </div>
-          );
+                  <g
+                    id="Group_12328"
+                    data-name="Group 12328"
+                    transform="translate(4.184 4.264)"
+                  >
+                    <line
+                      id="Line_888"
+                      data-name="Line 888"
+                      x2="12.851"
+                      transform="matrix(0.695, -0.719, 0.719, 0.695, 0.158, 9.244)"
+                      fill="none"
+                      stroke="#402cdd"
+                      stroke-linecap="round"
+                      stroke-width="1"
+                    />
+                    <line
+                      id="Line_889"
+                      data-name="Line 889"
+                      x2="12.851"
+                      transform="matrix(0.719, 0.695, -0.695, 0.719, 0, 0.158)"
+                      fill="none"
+                      stroke="#402cdd"
+                      stroke-linecap="round"
+                      stroke-width="1"
+                    />
+                  </g>
+                </g>
+              </svg>
+            </button>
+          </div>
+        );
 
-          if (notification.href) {
-            return (
-              <Link
-                key={notification.id}
-                href={notification.href}
-                className={notificationClasses}
-              >
-                {notificationContent}
-              </Link>
-            );
-          }
-
+        if (notification.href) {
           return (
+            <Link
+              key={notification.id}
+              href={notification.href}
+              className={notificationClasses}
+            >
+              {notificationContent}
+            </Link>
+          );
+        }
+
+        return (
+          <div
+            className="fixed  right-4 flex flex-col gap-3 pointer-events-none w-[90%]"
+            style={{
+              zIndex: `${index + 9999999999999999999}`,
+              top: `16px`,
+            }}
+          >
             <div key={notification.id} className={notificationClasses}>
               {notificationContent}
             </div>
-          );
-        })}
-      </div>
+          </div>
+        );
+      })}
     </>
   );
 };
