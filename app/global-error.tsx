c4b6394eeee0947e719cdaf9f736542bc83e6558
AlaaAsaadDev <alaa.asaad@ramaaz.com>
2025-07-12 13:22:58 +0300

    fix import styles

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -8,7 +8,7 @@ import {
   GeneralErrorIllustration,
   NetworkErrorIllustration,
 } from "../components/global/ErrorIllustrations";
-import "../styles/global.css";
+import "styles/globals.css";
 export default function GlobalError({ error, reset }) {
   const sendError = async () => {
     const userAgent =
