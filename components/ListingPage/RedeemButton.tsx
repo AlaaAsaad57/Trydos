2b92a827398db6ab0d83c79f342fb0beb9e7dab3
AlaaAsaadDev <alaa.asaad@ramaaz.com>
2025-07-21 13:29:47 +0300

    refactor: Update z-index in RedeemButton component for improved layering - Adjusted the z-index value in the RedeemButton component to ensure proper stacking order, enhancing visibility and interaction with overlapping elements.

--- components/ListingPage/RedeemButton.tsx
+++ components/ListingPage/RedeemButton.tsx
@@ -54,7 +54,7 @@ function RedeemButton() {
 
   return (
     <div
-      className="absolute pr-[5px] pl-[8px] text-nowrap flex-row h-[19px] gap-[2px] items-center  top-[-8px] left-[0px] z-10 rounded-tr-[4px] rounded-tl-[15px] rounded-bl-[4px] rounded-br-[15px] bg-[#FFF3E8] text-[#FF6200] text-[9px] medium min-w-[140px]"
+      className="absolute pr-[5px] pl-[8px] text-nowrap flex-row h-[19px] gap-[2px] items-center  top-[-8px] left-[0px] z-[99] rounded-tr-[4px] rounded-tl-[15px] rounded-bl-[4px] rounded-br-[15px] bg-[#FFF3E8] text-[#FF6200] text-[9px] medium min-w-[140px]"
       style={{
         border: "1px solid #FF6200",
       }}
