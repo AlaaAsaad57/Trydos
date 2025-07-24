0f7387ab194c8100520c097cac3e6f3f7d64b735
AlaaAsaadDev <alaa.asaad@ramaaz.com>
2025-07-24 13:18:19 +0300

    feat: Add product meta API endpoint and fix URL construction in ProductCard - Introduced a new API endpoint for fetching product meta data with a GET request to "/web/product/product-meta/{slug}" on the market server. - Updated URL construction in ProductCard to ensure query parameters are prefixed with a '?' for correct URL formatting.

--- components/Server/ProductCard.tsx
+++ components/Server/ProductCard.tsx
@@ -41,7 +41,7 @@ function ProductCard({
       searchParams.set("color", activeColor.color_name);
     }
     if (searchParams.values().toArray().length > 0) {
-      return url + searchParams.toString();
+      return url + `?${searchParams.toString()}`;
     } else {
       return url;
     }
