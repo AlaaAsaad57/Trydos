a4279b5e347c88079e7d1c63cf83ebb65e4016a1
AlaaAsaadDev <alaa.asaad@ramaaz.com>
2025-12-20 08:55:16 -0500

    fix: correct export syntax for API in treaty initialization

--- lib/eden/index.ts
+++ lib/eden/index.ts
@@ -1,4 +1,4 @@
 import { treaty } from "@elysiajs/eden";
 import type { AppType } from "../../app/api/[[...slugs]]/route";
 
-export const api = treaty<AppType>("loclhost:3000/api");
+export const { api } = treaty<AppType>("loclhost:3000/api");
