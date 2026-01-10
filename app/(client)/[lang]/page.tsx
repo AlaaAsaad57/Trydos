cadb869066483c9d24f654baa5f5668cb0fc72f6
AlaaAsaadDev <alaa.asaad@ramaaz.com>
2026-01-10 04:47:11 -0500

    un comment page

--- app/(client)/[lang]/page.tsx
+++ app/(client)/[lang]/page.tsx
@@ -55,7 +55,58 @@ async function HomePage({ params, searchParams }) {
           }  bg-white w-full pl-[10px] shadow-[0px_0px_6px_rgb(0,0,0,0.1)] z-[999999995]`}
         >
           <SearchIcon country={country} language={language} />
+          <Suspense
+            fallback={<MobileNavigationSkeleton />}
+            key={`Navbar ${lang}`}
+          >
+            {/*@ts-expect-error Async Server Component is valid in Next  */}
+            <MainCategoriesNavbar lang={lang} mainCategory={mainCategory} />
+          </Suspense>
         </div>
+
+        <Suspense fallback={<StoriesSkeleton />} key={`Stories ${lang}`}>
+          {/*@ts-expect-error Async Server Component is valid in Next  */}
+          <StoriesBarServer
+            language={lang.split("-")[1]}
+            country={lang.split("-")[0]}
+          />
+        </Suspense>
+
+        <Suspense
+          fallback={<FeaturedProductsSkeleton />}
+          key={`Featured Products ${lang} ${mainCategory ?? "main"}`}
+        >
+          {/*@ts-expect-error Async Server Component is valid in Next  */}
+          <FeaturedProductWrapper
+            currency={currency}
+            lang={lang}
+            mainCategory={mainCategory}
+          />
+        </Suspense>
+        <Suspense
+          fallback={<FeaturedProductsSkeleton />}
+          key={`FlashDeals ${lang} ${mainCategory ?? "main"}`}
+        >
+          {/*@ts-expect-error Async Server Component is valid in Next  */}
+
+          <FlashProductWrapper
+            currency={currency}
+            lang={lang}
+            mainCategory={mainCategory}
+          />
+        </Suspense>
+        <Home key={`Home ${lang}`} />
+        <Suspense
+          fallback={<OfferListSkeleton />}
+          key={`OfferList ${lang} ${mainCategory ?? "main"}`}
+        >
+          {/*@ts-expect-error Async Server Component is valid in Next  */}
+          <BoutiquesListWrapper
+            currency={currency}
+            params={{ lang: lang }}
+            mainCategory={mainCategory}
+          />
+        </Suspense>
       </>
     );
   } catch (error) {
