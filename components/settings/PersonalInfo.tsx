b343b4a66181cae2765acab39324757db9c41c1b
AlaaAsaadDev <alaa.asaad@ramaaz.com>
2026-01-14 06:04:47 -0500

    fix deleted required data

---
+++ components/settings/PersonalInfo.tsx
@@ -0,0 +1,29 @@
+import ConfirmMobileChange from "./ConfirmMobileChange";
+import XIcon from "public/svg/Xicon";
+export const ConfirmationModal = ({
+  closeWindow,
+  value,
+  successCallback,
+  forVerify,
+}: any) => {
+  return (
+    <>
+      <XIcon
+        className="w-[20px] absolute z-30 top-[calc(50%-170px)]  right-[30px]  h-[20px] cursor-pointer"
+        onClick={closeWindow}
+      />
+      <div className="fixed z-20 top-0 left-0  w-full h-full bg-black opacity-50" />
+
+      <div className="p-5 flex  w-auto justify-center z-30 h-auto absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white rounded-[15px]">
+        <ConfirmMobileChange
+          forVerify={forVerify}
+          closeWindow={closeWindow}
+          value={value}
+          successCallbackFunction={(idToken) => {
+            successCallback(idToken);
+          }}
+        />
+      </div>
+    </>
+  );
+};
