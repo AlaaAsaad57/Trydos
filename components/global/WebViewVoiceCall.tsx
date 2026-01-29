a965a0f70c038ad02d6ce23901e6d1b317687b25
AlaaAsaadDev <alaa.asaad@ramaaz.com>
2026-01-29 08:45:11 -0500

    fix show image of user in webview calls

--- components/global/WebViewVoiceCall.tsx
+++ components/global/WebViewVoiceCall.tsx
@@ -7,6 +7,7 @@ import AgoraRTC, {
 } from "agora-rtc-react";
 import { useStopwatch } from "react-timer-hook";
 import { getTwoLetters } from "components/Chat/chatsFunctions";
+import { GetImageUrl } from "utils/tinyUtils";
 
 const config: any = {
   mode: "rtc",
@@ -188,7 +189,7 @@ function WebViewVoiceCall(props) {
               <div
                 className="hgg"
                 style={{
-                  backgroundImage: `url(${props.active})`,
+                  backgroundImage: `url(${GetImageUrl(props.active)})`,
                   left: 0,
                   right: 0,
                   margin: "0 auto",
