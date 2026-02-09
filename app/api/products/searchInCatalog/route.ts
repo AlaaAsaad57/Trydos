d5e03ebc69d72e75453d2e01143b4be89af0282a
AlaaAsaadDev <alaa.asaad@ramaaz.com>
2026-02-09 06:42:59 -0500

    fix: correct variable naming for user ID and add recommended offset in product search parameters

--- app/api/products/searchInCatalog/route.ts
+++ app/api/products/searchInCatalog/route.ts
@@ -18,7 +18,7 @@ export async function GET(req: NextRequest) {
     const country = req.headers.get("country")?.trim() || "sy";
     let language = req.headers.get("language")?.trim();
     const lang = req.headers.get("lang")?.trim();
-    const user_id = req.headers.get("uid")?.trim();
+    const userId = req.headers.get("uid")?.trim();
     language = language ?? lang ?? "en";
 
     const searchParams = req.nextUrl.searchParams;
@@ -73,7 +73,8 @@ export async function GET(req: NextRequest) {
       filters_offset: Number(searchParams.get("filters_offset") || 1),
       country,
       language_code: language,
-      userId: user_id || null,
+      user_id: userId,
+      recommended_offset: Number(searchParams.get("recommended_offset") || 0),
     };
     const result = await getProductsAndFiltersFromElastic(params);
 
