3711564133c261ff08990835e277360844d7d354
AlaaAsaadDev <alaa.asaad@ramaaz.com>
2026-04-10 17:45:56 -0400

    Simplify robots.txt rules to disallow all access

--- app/robots.ts
+++ app/robots.ts
@@ -4,30 +4,7 @@ import { General_Site_Data } from "serverRequests/meta/StructuredData/Constants"
 export default function robots(): MetadataRoute.Robots {
   const baseUrl = General_Site_Data.url;
   return {
-    rules: [
-      {
-        userAgent: "*",
-        allow: "*",
-        disallow: [
-          "/api/",
-          "/settings",
-          "/dashboard/",
-          "/_next/",
-          "/static/",
-          "/private/",
-          "/temp/",
-          "/test/",
-          "/staging/",
-          "/dev/",
-          "/revalidate/",
-          "/callInProg/",
-          "/endCall/",
-          "/call_direct/",
-          "/selectCountry/",
-          "/api-test/",
-        ],
-      },
-    ],
+    rules: [{ userAgent: "*", disallow: "/" }],
     sitemap: `${baseUrl}/sitemap.xml`,
     host: baseUrl,
   };
