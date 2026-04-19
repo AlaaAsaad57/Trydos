00900c3c81f243583a6f262bf310496c04538965
AlaaAsaadDev <alaa.asaad@ramaaz.com>
2026-04-19 07:41:43 -0400

    Add sourcemaps configuration to disable source map generation in Sentry

--- next.config.ts
+++ next.config.ts
@@ -126,6 +126,9 @@ export default withSentryConfig(nextConfig, {
   // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
 
+  sourcemaps: {
+    disable: true,
+  },
   // For all available options, see:
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
