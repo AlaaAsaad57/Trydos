9c4a3e6bbe186b7de5ce9216a4c23cac8c298cc7
AlaaAsaadDev <alaa.asaad@ramaaz.com>
2026-05-06 10:42:00 +0300

    Update environment configuration and modify menu behavior for wishlist toggle

--- components/Home/Menu.tsx
+++ components/Home/Menu.tsx
@@ -217,7 +217,7 @@ const Menu = ({ user, setMenuOpen }) => {
               //   value: GA_CLICK_EVENT_VALUES.WISHLIST_BUTTON,
               // });
               setShowWishList(!showWishList);
-              setMenuOpen(false);
+              // setMenuOpen(false);
             }}
             icon={
               <MenuIcon>
