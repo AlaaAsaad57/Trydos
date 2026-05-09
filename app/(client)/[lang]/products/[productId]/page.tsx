64f6949fd097653281af2015615aa3af18274743
AlaaAsaadDev <alaa.asaad@ramaaz.com>
2026-05-09 11:49:46 +0300

    Comment out unused generateMetadata function for future reference and cleaner code

--- app/(client)/[lang]/products/[productId]/page.tsx
+++ app/(client)/[lang]/products/[productId]/page.tsx
@@ -7,39 +7,39 @@ import { LogServerError } from "utils/serverErrorReporter";
 import ProductPageContent from "components/Product/ProductPageContent";
 import { Metadata } from "next";
 
-export async function generateMetadata({ params, searchParams }) {
-  let [Params, SearchParams] = await Promise.all([params, searchParams]);
-  let [country, language] = Params.lang.split("-");
-  try {
-    const metaData = await GetProductMeta({
-      country: country,
-      language: language,
-      slug: Params.productId,
-      searchParams: SearchParams,
-    });
+// export async function generateMetadata({ params, searchParams }) {
+//   let [Params, SearchParams] = await Promise.all([params, searchParams]);
+//   let [country, language] = Params.lang.split("-");
+//   try {
+//     const metaData = await GetProductMeta({
+//       country: country,
+//       language: language,
+//       slug: Params.productId,
+//       searchParams: SearchParams,
+//     });
 
-    // @ts-ignore
-    if (metaData?.error || !metaData) {
-      // @ts-ignore
-      throw new Error(metaData?.error ?? metaData);
-    }
-    RedisSet(`${Params.productId}-${Params.lang}`, JSON.stringify(metaData));
+//     // @ts-ignore
+//     if (metaData?.error || !metaData) {
+//       // @ts-ignore
+//       throw new Error(metaData?.error ?? metaData);
+//     }
+//     RedisSet(`${Params.productId}-${Params.lang}`, JSON.stringify(metaData));
 
-    return metaData;
-  } catch (error) {
-    LogServerError(
-      {
-        error,
-        type: "get product meta error",
-        country,
-        language,
-        product_slug: Params.productId,
-      },
-      `/${country}-${language}/featured`,
-    );
-    redirect(`/${country}-${language}?message=product_not_found`);
-  }
-}
+//     return metaData;
+//   } catch (error) {
+//     LogServerError(
+//       {
+//         error,
+//         type: "get product meta error",
+//         country,
+//         language,
+//         product_slug: Params.productId,
+//       },
+//       `/${country}-${language}/featured`,
+//     );
+//     redirect(`/${country}-${language}?message=product_not_found`);
+//   }
+// }
 export const metadata: Metadata = {
   title: "TRYDOS-TEST",
   description: "TRYDOS-TEST",
