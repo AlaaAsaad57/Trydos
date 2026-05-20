7612b29e00e6d82a0d28e7fec565d728dee3ed67
AlaaAsaadDev <alaa.asaad@ramaaz.com>
2026-05-20 11:01:11 +0300

    Add responsive layout and suspense components to HomePage

--- app/(client)/[lang]/page.tsx
+++ app/(client)/[lang]/page.tsx
@@ -82,7 +82,20 @@ async function HomePage({ params, searchParams }) {
     return (
       <>
         {/* <StructuredDataScript lang={lang} /> */}
-      
+        <div
+          className={`${
+            isRtl ? "flex-row-reverse pr-[10px]" : "flex-row pl-[10px]"
+          }  bg-white w-full pl-[10px] shadow-[0px_0px_6px_rgb(0,0,0,0.1)] z-999999995`}
+        >
+          <SearchIcon country={country} language={language} />
+          <Suspense
+            fallback={<MobileNavigationSkeleton />}
+            key={`Navbar ${lang}`}
+          >
+            {/*@ts-expect-error Async Server Component is valid in Next  */}
+            <MainCategoriesNavbar lang={lang} mainCategory={mainCategory} />
+          </Suspense>
+        </div>
         <Suspense fallback={<StoriesSkeleton />} key={`Stories ${lang}`}>
           {/*@ts-expect-error Async Server Component is valid in Next  */}
           <StoriesBarServer
@@ -91,7 +104,41 @@ async function HomePage({ params, searchParams }) {
           />
         </Suspense>
 
- 
+        <Suspense
+          fallback={<FeaturedProductsSkeleton />}
+          key={`Featured Products ${lang} ${mainCategory ?? "main"}`}
+        >
+          {/*@ts-expect-error Async Server Component is valid in Next  */}
+          <FeaturedProductWrapper
+            currency={currency}
+            lang={lang}
+            mainCategory={mainCategory}
+          />
+        </Suspense>
+        <Suspense
+          fallback={<FeaturedProductsSkeleton />}
+          key={`FlashDeals ${lang} ${mainCategory ?? "main"}`}
+        >
+          {/*@ts-expect-error Async Server Component is valid in Next  */}
+
+          <FlashProductWrapper
+            currency={currency}
+            lang={lang}
+            mainCategory={mainCategory}
+          />
+        </Suspense>
+        <Home key={`Home ${lang}`} />
+        <Suspense
+          fallback={<OfferListSkeleton />}
+          key={`OfferList ${lang} ${mainCategory ?? "main"}`}
+        >
+          {/*@ts-expect-error Async Server Component is valid in Next  */}
+          <BoutiquesListWrapper
+            currency={currency}
+            params={{ lang: lang }}
+            mainCategory={mainCategory}
+          />
+        </Suspense>
       </>
     );
   } catch (error) {
