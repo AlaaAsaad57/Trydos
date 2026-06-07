ba926a4edc6544ca398f51ad8e0903ba4ecd578e
AlaaAsaadDev <alaa.asaad@ramaaz.com>
2026-06-06 21:45:58 -0800

    comment function in robots.txt so bots can crawl the page and see the noindex meta

--- app/robots.ts
+++ app/robots.ts
@@ -5,16 +5,16 @@ import { General_Site_Data } from "serverRequests/meta/StructuredData/Constants"
 export default function robots(): MetadataRoute.Robots {
   // Only the production domain (NEXT_PUBLIC_ALLOW_INDEXING=true) is crawlable.
   // Everywhere else, disallow the whole site so dev/preview builds stay out of search.
-  if (!isIndexingAllowed()) {
-    return {
-      rules: [
-        {
-          userAgent: "*",
-          disallow: "/",
-        },
-      ],
-    };
-  }
+  // if (!isIndexingAllowed()) {
+  //   return {
+  //     rules: [
+  //       {
+  //         userAgent: "*",
+  //         disallow: "/",
+  //       },
+  //     ],
+  //   };
+  // }
   return {
     rules: [
       {
