8f5cb599aba81c0989fa04c704f968b8e12a196f
AlaaAsaadDev <alaa.asaad@ramaaz.com>
2026-06-13 02:56:29 -0800

    edit button More wich have "Share" to "More"

--- components/products/ShareOptions.tsx
+++ components/products/ShareOptions.tsx
@@ -341,7 +341,7 @@ function ShareOptions({ product }: any) {
               <line x1="15.41" y1="6.51" x2="8.59" y2="10.49" />
             </svg>
           </div>
-          <div className="share-name">{translateFunction("Share")}</div>
+          <div className="share-name">{translateFunction("More")}</div>
         </div>
       )}
       {getUserChat() &&
